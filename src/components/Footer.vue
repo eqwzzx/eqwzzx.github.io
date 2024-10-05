36e36df4e04cab96eca591774c1a7f6eb67a95aa
eqwzzx <eqwzzx@list.ru>
2024-10-06 00:35:46 +0600

    Create Footer.vue
    Signed-off-by: eqwzzx <eqwzzx@list.ru>

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <footer class="text-left ml-5 font-bold mt-52">
+    <p>&copy; {{ new Date().getFullYear() }} eqwzzx. All rights reserved.</p>
+    <p>Веб-сайт сделан
+      <a class="no-underline text-gray-700 hover:underline" href="https://luckyloo.cloud">
+        <span class="text-gray-500 hover:text-gray-700">@LuckyLoo_</span>
+      </a>
+    </p>
+  </footer>
+</template>
