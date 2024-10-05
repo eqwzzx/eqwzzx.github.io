219ca7c6bca64f5eaf6b12b3d2be76177a3a9a03
eqwzzx <eqwzzx@list.ru>
2024-10-06 00:36:03 +0600

    Add files via upload
    Signed-off-by: eqwzzx <eqwzzx@list.ru>

---
+++ src/components/Main.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+<h1 class="font-bold text-center text-3xl">Welcome to eqwzzx's Portfolio</h1>
+</template>
---
+++ src/components/Projects.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+  <div class="flex flex-col lg:flex-row gap-8 lg:gap-24 items-center justify-center w-full">
+    <div class="flex flex-col justify-center w-60 h-60 gap-2.5 transition duration-300 mt-5 cursor-pointer border-2 border-solid border-gray-200 p-4 hover:border-sky-500 hover:transform hover:scale-105">
+      <p class="font-bold text-center">Projects</p>
+      <p>Pulsar- multipurpose bot for your discord server</p>
+    </div>
+
+    <div class="flex flex-col justify-center w-60 h-60 gap-2.5 transition duration-300 mt-5 cursor-pointer border-2 border-solid border-gray-200 p-4 hover:border-sky-500 hover:transform hover:scale-105">
+      <p class="font-bold text-center">About me</p>
+      <p>Just a guy from Kazakhstan</p>
+    </div>
+
+    <div class="flex flex-col justify-center w-60 h-60 gap-2.5 transition duration-300 mt-5 cursor-pointer border-2 border-solid border-gray-200 p-4 hover:border-sky-500 hover:transform hover:scale-105">
+      <p class="font-bold text-center">Contacts</p>
+      <a class="decoration-0 hover:underline" href="mailto:root@eqwzzx.fun">root@eqwzzx.fun</a>
+      <a class="decoration-0 hover:underline" href="https://discord.com/users/808641359006400512">Discord</a>
+      <a class="decoration-0 hover:underline" href="https://github.com/eqwzzx/">GitHub</a>
+    </div>
+  </div>
+</template>
