889b1ed566f14f31d675efcda3951d36028860d8
eqwzzx <eqwzzx@list.ru>
2024-10-06 00:32:26 +0600

    Create App.vue
    Signed-off-by: eqwzzx <eqwzzx@list.ru>

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Footer from './components/Footer.vue'
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Projects from './components/Projects.vue'
+</script>
+
+<template>
+  <Header />
+  <Main />
+  <Projects />
+  <Footer />
+</template>
