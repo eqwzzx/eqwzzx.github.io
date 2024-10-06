743c83a587ff656462dc64d3fc95ad0134a5005b
eqwzzx <eqwzzx@list.ru>
2024-10-06 18:12:14 +0600

    Add files via upload
    Signed-off-by: eqwzzx <eqwzzx@list.ru>

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<script setup>
+import Footer from '../components/footer.vue'
+
+const projects = () => {
+  useRouter().push("/projects");
+}
+
+const contacts = () => {
+  useRouter().push("/contact");
+}
+</script>
+<template>
+  <div class="font-montserrat">
+    <header>
+      <h2 class="font-bold text-left ml-5 mt-5 text-2xl">eqwzzx</h2>
+      <img class="mx-auto rounded-2xl h-40 w-40 hover:transform hover:scale-105 hover:cursor-grab active:cursor-grabbing hover:transition hover:duration-300 transition duration-300" src="https://i.imgur.com/DpWYj2j.png" alt="pfp">
+    </header>
+    <main>
+      <h1 class="font-bold text-center text-3xl mt-24 lg:mt-5">Welcome to eqwzzx's Portfolio</h1>
+      <p class="text-center mt-2">Hi! I'm eqwzzx (Fish), I'm just starting out web development. I'm from Kazakhstan.</p>
+      <div class="flex flex-col md:flex-row justify-center items-center mt-24 gap-24">
+        <div @click="projects" class="flex rounded-2xl flex-col border-2 border-gray-500 h-40 text-md w-40 text-center border-solid p-4 transition hover:transform hover:scale-105 hover:transition hover:duration-200 hover:border-sky-500 hover:-rotate-12 cursor-pointer">
+          <h4 class="font-bold">Projects</h4>
+          <p class="mt-5">List of all of my projects</p>
+        </div>
+        <div @click="contacts" class="flex rounded-2xl flex-col border-2 border-gray-500 h-40 text-md w-40 text-center border-solid p-4 transition hover:transform hover:scale-105 hover:transition hover:duration-200 hover:border-sky-500 hover:-rotate-12 cursor-pointer">
+          <h4 class="font-bold">Contact</h4>
+          <p class="mt-5">My social media, etc</p>
+        </div>
+      </div>
+    </main>
+    <Footer />
+  </div>
+</template>
---
+++ pages/projects.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const router = useRouter();
+
+const goBack = () => {
+  router.push("/");
+}
+</script>
+
+<template>
+  <div class="font-montserrat">
+  <h1 class="font-bold text-3xl mt-7 text-center">Projects</h1>
+  <p class="text-center mt-5 text-lg">All of my projects</p>
+    <div class="flex flex-col md:flex-row justify-center items-center mt-24 gap-24">
+      <div @click="" class="flex rounded-2xl flex-col border-2 border-gray-500 h-40 text-md w-60 text-center border-solid p-4 transition hover:transform hover:scale-105 hover:transition hover:duration-200 cursor-pointer">
+        <h4 class="font-bold">Pulsar</h4>
+        <p class="mt-5">Multipurpose discord bot for your discord server</p>
+        <p class="mt-2 text-sm text-green-400 font-mono">Done</p>
+      </div>
+<!--      <div @click="" class="flex rounded-2xl flex-col border-2 border-gray-500 h-40 text-md w-60 text-center border-solid p-4 transition hover:transform hover:scale-105 hover:transition hover:duration-200 cursor-pointer">-->
+<!--        <h4 class="font-bold">404 Not found</h4>-->
+<!--        <p class="mt-5">Soon...</p>-->
+<!--        <p class="mt-2 text-sm text-orange-400 font-mono">In development</p>-->
+<!--      </div>-->
+    </div>
+    <div class="flex justify-center">
+      <button @click="goBack" class="mt-24 bg-gray-700 p-4 rounded-lg text-white">Go back</button>
+    </div>
+  </div>
+</template>
+
+<!--
+<p class="mt-2 text-sm text-orange-400 font-mono">In development</p>
+<p class="mt-2 text-sm text-green-400 font-mono">Done</p>
+<p class="mt-2 text-sm text-red-400 font-mono">Not supported</p>
+-->
